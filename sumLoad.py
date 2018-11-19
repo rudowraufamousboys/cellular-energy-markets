4b52e8997664a74eacce077f2819db3e73eef356
rudowraufamousboys <44801735+rudowraufamousboys@users.noreply.github.com>
2018-11-19 10:47:45 +0100

    Update sumLoad.py

--- sumLoad.py
+++ sumLoad.py
@@ -4,21 +4,17 @@ load=cellC4.dfload
 
 dropPriceofLoad=load.drop('price', axis=0)
 
-i=len(dropPriceofLoad)
-
 for index, row in dropPriceofLoad.iterrows():
     
     sum=dropPriceofLoad.sum(axis=1)
     sumLoad.append(sum)
     
-    if i == len(dropPriceofLoad):
-        break
+    break
     
 k=pd.Series(sumLoad)
  
 r=k.to_dict() 
 
 sumLoaddf=pd.DataFrame.from_dict(r, orient='columns')
-sumLoaddf=sumLoaddf.rename()
 
   
